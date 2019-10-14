863cbe1f7799ef8eecaa65fc6395755e8f4793fa
Mohammed Anas Khatri <mohammed.khatri@hmcts.net>
2019-10-14 10:53:18 +0100

    First cut of nested complex and collection CaseFields sanitisation

--- src/shared/services/form/field-type-sanitiser.spec.ts
+++ src/shared/services/form/field-type-sanitiser.spec.ts
@@ -1,5 +1,6 @@
 import { FieldTypeSanitiser } from './field-type-sanitiser';
 import { CaseField } from '../../domain/definition';
+import { createCaseField, createFieldType } from '../../fixture';
 
 describe('FieldTypeSanitiser', () => {
 
@@ -16,6 +17,23 @@ describe('FieldTypeSanitiser', () => {
     '              {\n' +
     '                "code": "M",\n' +
     '                "label": "Male"\n' +
+    '              }' +
+    '            ]\n' +
+    '          }');
+
+  const COMPLEX_VALUE_DYNAMIC_LIST = JSON.parse('{\n' +
+    '            "value": {\n' +
+    '              "code": "List1",\n' +
+    '              "label": "List 1"\n' +
+    '            },\n' +
+    '            "list_items": [\n' +
+    '              {\n' +
+    '                "code": "List1",\n' +
+    '                "label": "List 1"\n' +
+    '              },\n' +
+    '              {\n' +
+    '                "code": "List2",\n' +
+    '                "label": "List 2"\n' +
     '              }' +
     '            ]\n' +
     '          }');
@@ -42,6 +60,42 @@ describe('FieldTypeSanitiser', () => {
       dynamicList: 'M'
     }
   }
+  let  dynamicPostCodeList:CaseField = Object.assign(new CaseField(), {
+    id: 'dynamicPostCodeList',
+    label: 'DynamicPostCodeList',
+    value: VALUE_DYNAMIC_LIST,
+    hint_text: null,
+    field_type: {
+      id: 'dynamicPostCodeList',
+      type: 'DynamicList',
+      min: null,
+      max: null,
+      regular_expression: null,
+      fixed_list_items: [],
+      complex_fields: [],
+      collection_field_type: null
+    },
+    security_label: 'PUBLIC',
+    order: null,
+    display_context: null,
+    show_condition: null,
+    show_summary_change_option: null,
+    show_summary_content_option: null
+  });
+
+  let  complexCaseField:CaseField = createCaseField('finalReturn', 'Final return', '',
+    createFieldType('Return', 'Complex', [
+      createCaseField('addressAttended',
+        'Address Attended',
+        'Address Attended hint text',
+        createFieldType('AddressUK', 'Complex', [
+          createCaseField('AddressLine1', 'Building and Street', 'hint 1', createFieldType('TextMax150', 'Text', []), null, 2),
+          createCaseField('AddressLine2', '', 'hint 2', createFieldType('TextMax50', 'Text', []), null),
+          dynamicPostCodeList
+        ]),
+        null
+      )
+    ]), 'COMPLEX');
 
   const caseFields: CaseField[] = [
     Object.assign(new CaseField(), {
@@ -88,6 +142,7 @@ describe('FieldTypeSanitiser', () => {
       show_summary_change_option: null,
       show_summary_content_option: null
     }),
+    complexCaseField,
     Object.assign(new CaseField(), {
       id: 'dynamicList',
       label: 'DynamicList',
--- src/shared/services/form/field-type-sanitiser.ts
+++ src/shared/services/form/field-type-sanitiser.ts
@@ -12,11 +12,22 @@ export class FieldTypeSanitiser {
    * @param editForm
    */
    sanitiseLists(caseFields: CaseField[], editForm: any) {
-
     this.getDynamicListsFromCaseFields(caseFields).forEach(dynamicField => {
-      this.getListOfKeysFromEditForm(editForm).forEach((key) => {
-        this.createValueCodePairAlongWithListIfKeyExistsInForm(dynamicField, key, editForm);
-      });
+      if(dynamicField.field_type.type === 'DynamicList') {
+        this.getDeepKeys(editForm).filter(key => key.endsWith(dynamicField.id)).forEach((key) => {
+          let formValue = this.getNestedValue(editForm, key)
+          let value = {
+            value: this.getMatchingCodeFromListOfItems(dynamicField, formValue, key),
+            list_items: dynamicField.list_items
+          };
+          this.setValue(key, value, editForm);
+        });
+
+      } else if (dynamicField.field_type.type === 'Collection') {
+         this.sanitiseLists(dynamicField.field_type.collection_field_type.complex_fields, editForm);
+      } else if (dynamicField.field_type.type === 'Complex') {
+        this.sanitiseLists(dynamicField.field_type.complex_fields, editForm);
+      }
     });
   }
 
@@ -30,7 +41,8 @@ export class FieldTypeSanitiser {
   }
 
   private getMatchingCodeFromListOfItems(dynamicField: CaseField, editForm: any, key) {
-    let result = dynamicField.list_items.filter(value => value.code === editForm['data'][key]);
+
+    let result = dynamicField.list_items.filter(value => value.code === editForm);
     return result.length > 0 ? result[0] : {};
   }
 
@@ -40,6 +52,49 @@ export class FieldTypeSanitiser {
 
   private getDynamicListsFromCaseFields(caseFields: CaseField[]): CaseField[] {
     return caseFields
-      .filter(caseField => caseField.field_type.type === 'DynamicList');
+      .filter(caseField => (caseField.field_type.type === 'Collection' ||
+        caseField.field_type.type === 'Complex' ||
+        caseField.field_type.type === 'DynamicList'));
+  }
+
+  private getDeepKeys(obj) {
+    var keys = [];
+    for(var key in obj) {
+      keys.push(key);
+      if(typeof obj[key] === "object") {
+        var subkeys = this.getDeepKeys(obj[key]);
+        keys = keys.concat(subkeys.map(function(subkey) {
+          return key + "." + subkey;
+        }));
+      }
+    }
+    return keys;
+  }
+
+  private getNestedValue(obj, key) {
+    return key.split(".").reduce(function(result, key) {
+      return result[key]
+    }, obj);
+  }
+
+  private setValue(propertyPath, value, obj) {
+
+    // this is a super simple parsing, you will want to make this more complex to handle correctly any path
+    // it will split by the dots at first and then simply pass along the array (on next iterations)
+    let properties = Array.isArray(propertyPath) ? propertyPath : propertyPath.split(".")
+
+    // Not yet at the last property so keep digging
+    if (properties.length > 1) {
+      // The property doesn't exists OR is not an object (and so we overwritte it) so we create it
+      if (!obj.hasOwnProperty(properties[0]) || typeof obj[properties[0]] !== "object") obj[properties[0]] = {}
+      // We iterate.
+      return this.setValue(properties.slice(1), value, obj[properties[0]])
+      // This is the last property - the one where to set the value
+    } else {
+      // We set the value to the last property
+      obj[properties[0]] = value
+      return true // this is the end
+    }
+
   }
 }
